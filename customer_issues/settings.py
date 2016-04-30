44728d761528242d9aed730794bbb4ab43f3f39f
Krishna Regmi <kisna72@gmail.com>
2016-04-30 16:23:28 -0500

    update

--- customer_issues/settings.py
+++ customer_issues/settings.py
@@ -83,10 +83,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT =  os.path.join(BASE_DIR,'static')
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'issues' , "static")
+#     #'/var/www/static/',
+#     #'/Users/krishnaregmi/workdir/knowledge_management_project/customer_issues/issues',
+# ]
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-    '/Users/krishnaregmi/workdir/knowledge_management_project/customer_issues/issues',
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'issues' , "static")
+    os.path.join(PROJECT_ROOT, 'static'),
+)
