d3aa48a57aeb0184cd60ceb5fab86d528953bcb9
Krishna Regmi <kisna72@gmail.com>
2016-04-30 16:49:53 -0500

    se

--- customer_issues/settings.py
+++ customer_issues/settings.py
@@ -66,6 +66,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
